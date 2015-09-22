c51662927c9fc4eb4611a936d986f04c38de0a04
callummcgregor <callum@mcgregorfamily.org.uk>
2015-09-22 14:36:04 +0100

    Added RGB colours to this cube with help from the StackOverflow questions http://stackoverflow.com/questions/10330342/threejs-assign-different-colors-to-each-vertex-in-a-geometry

--- Development/3D Cube/cube.js
+++ Development/3D Cube/cube.js
@@ -18,9 +18,41 @@ function onLoad() {
     world.renderer.setClearColor(0x6d6d6d);
     document.body.appendChild(world.renderer.domElement);
 
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial( { color: 0xff00ff } );
-    var cube = new THREE.Mesh(geometry, material);
+    // this material causes a mesh to use colors assigned to vertices
+    var vertexColorMaterial = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
+
+    var color, point, face, numberOfSides, vertexIndex;
+
+// faces are indexed using characters
+    var faceIndices = [ 'a', 'b', 'c', 'd' ];
+
+    var size = 1;
+    var cubeGeometry = new THREE.CubeGeometry( size, size, size );
+
+// first, assign colors to vertices as desired
+    for ( var i = 0; i < cubeGeometry.vertices.length; i++ )
+    {
+        point = cubeGeometry.vertices[ i ];
+        color = new THREE.Color( 0xffffff );
+        color.setRGB( 0.5 + point.x / size, 0.5 + point.y / size, 0.5 + point.z / size );
+        cubeGeometry.colors[i] = color; // use this array for convenience
+    }
+
+// copy the colors to corresponding positions
+//     in each face's vertexColors array.
+    for ( var i = 0; i < cubeGeometry.faces.length; i++ )
+    {
+        face = cubeGeometry.faces[ i ];
+        numberOfSides = ( face instanceof THREE.Face3 ) ? 3 : 4;
+        for( var j = 0; j < numberOfSides; j++ )
+        {
+            vertexIndex = face[ faceIndices[ j ] ];
+            face.vertexColors[ j ] = cubeGeometry.colors[ vertexIndex ];
+        }
+    }
+
+    cube = new THREE.Mesh( cubeGeometry, vertexColorMaterial );
+
     world.scene.add(cube);
 
     world.camera.position.z = 5;
