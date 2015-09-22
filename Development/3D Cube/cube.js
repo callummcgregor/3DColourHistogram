cbd02aaca6e1fbdeda8f6a5069f750d831030334
callummcgregor <callum@mcgregorfamily.org.uk>
2015-09-22 15:00:07 +0100

    Refactoring code and addings to development log

--- Development/3D Cube/cube.js
+++ Development/3D Cube/cube.js
@@ -18,44 +18,42 @@ function onLoad() {
     world.renderer.setClearColor(0x6d6d6d);
     document.body.appendChild(world.renderer.domElement);
 
-    // this material causes a mesh to use colors assigned to vertices
-    var vertexColorMaterial = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
+    var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
 
     var color, point, face, numberOfSides, vertexIndex;
 
-// faces are indexed using characters
+    // faces are indexed using characters
     var faceIndices = [ 'a', 'b', 'c', 'd' ];
 
     var size = 1;
-    var cubeGeometry = new THREE.CubeGeometry( size, size, size );
+    var geometry = new THREE.BoxGeometry( size, size, size );
 
-// first, assign colors to vertices as desired
-    for ( var i = 0; i < cubeGeometry.vertices.length; i++ )
-    {
-        point = cubeGeometry.vertices[ i ];
+    // first, assign colors to vertices as desired
+    for ( var i = 0; i < geometry.vertices.length; i++ ) {
+        point = geometry.vertices[ i ];
         color = new THREE.Color( 0xffffff );
         color.setRGB( 0.5 + point.x / size, 0.5 + point.y / size, 0.5 + point.z / size );
-        cubeGeometry.colors[i] = color; // use this array for convenience
+        geometry.colors[i] = color; // use this array for convenience
     }
 
-// copy the colors to corresponding positions
-//     in each face's vertexColors array.
-    for ( var i = 0; i < cubeGeometry.faces.length; i++ )
+    // copy the colors to corresponding positions
+    //     in each face's vertexColors array.
+    for ( var i = 0; i < geometry.faces.length; i++ )
     {
-        face = cubeGeometry.faces[ i ];
+        face = geometry.faces[ i ];
         numberOfSides = ( face instanceof THREE.Face3 ) ? 3 : 4;
         for( var j = 0; j < numberOfSides; j++ )
         {
             vertexIndex = face[ faceIndices[ j ] ];
-            face.vertexColors[ j ] = cubeGeometry.colors[ vertexIndex ];
+            face.vertexColors[ j ] = geometry.colors[ vertexIndex ];
         }
     }
 
-    cube = new THREE.Mesh( cubeGeometry, vertexColorMaterial );
+    cube = new THREE.Mesh( geometry, material );
 
     world.scene.add(cube);
 
-    world.camera.position.z = 5;
+    world.camera.position.z = 2;
 
     render();
 }
