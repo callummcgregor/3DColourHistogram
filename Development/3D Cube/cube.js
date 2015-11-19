e042b52ebaed192900f43a9956a7b4909b6ec76b
callummcgregor <callum@mcgregorfamily.org.uk>
2015-11-19 14:25:46 +0000

    An images colours can now be viewed in the colour histogram. Still issues concerning HTML layout, pixel sampling rate, and neatness of code

--- Development/3D Cube/cube.js
+++ Development/3D Cube/cube.js
@@ -7,21 +7,74 @@
  * Initialises the world and initial objects within it
  */
 function onLoad() {
+    // Set up the Three.js variables, encapsulated in a world object
+    //var canvasWidth = window.innerWidth;
+    //var canvasHeight = window.innerHeight;
+    var canvasWidth = document.getElementById("histogram_holder").offsetWidth;
+    var canvasHeight = document.getElementById("histogram_holder").offsetHeight;
+
     var world = { }; // Variable to hold state of the 3D rendering world
-
     world.scene = new THREE.Scene();
-    world.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+    world.camera = new THREE.PerspectiveCamera( 75, canvasWidth / canvasHeight, 0.1, 1000 );
     world.controls = new THREE.OrbitControls( world.camera );
-
     world.renderer = new THREE.WebGLRenderer();
-    world.renderer.setSize( window.innerWidth, window.innerHeight );
+    world.renderer.setSize(canvasWidth, canvasHeight);
     world.renderer.setClearColor(0x6d6d6d);
-    document.body.appendChild( world.renderer.domElement );
-
     world.camera.position.z = 2;
 
-    var colors = [
+    // Append the Three.js rendered to the DOM
+    var canvas = document.getElementById("histogram_holder");
+    canvas.appendChild( world.renderer.domElement );
+
+    // Add event listener to the HTML5 file input
+    document.getElementById("file_input").addEventListener("change", function() {
+        var file = this.files[0]; // Only one file is being uploaded at a time
+
+        var imageCanvas = document.getElementById("image_canvas");
+        var context = imageCanvas.getContext("2d");
+
+        var image = new Image();
+        image.onload = function() {
+            var imageHeight = 20;//imageCanvas.height;
+            var imageWidth = 20;//imageCanvas.width;
+
+            context.drawImage(image, 0, 0, imageWidth, imageWidth);
+
+            console.log("Image width : ", imageWidth);
+            console.log("Image height: ", imageHeight);
+
+            var pixelCount = 0;
+            // Initiate array
+            // Each colour channel ranges from 0 to 255
+            var pixelColours = new Array(imageWidth * imageHeight);
+            for (var i = 0; i < pixelColours.length; i++) {
+                pixelColours[i] = new Array(3);
+            }
+
+            for (var x = 0; x < imageWidth; x++) {
+                for (var y = 0; y < imageHeight; y++) {
+                    var pixelData = context.getImageData(x, y, 1, 1).data;
+                    pixelColours[pixelCount][0] = pixelData[0];
+                    pixelColours[pixelCount][1] = pixelData[1];
+                    pixelColours[pixelCount][2] = pixelData[2];
+                    pixelCount++;
+                }
+            }
+
+            console.log("pixelCount: ", pixelCount);
+
+            var pixelColors16bit = applyColorQuantisation(pixelColours, 16);
+            plotColors(world, pixelColors16bit);
+        };
+
+        image.src = URL.createObjectURL(file); // Let this go to prevent memory leaks
+    }, false);
+
+    createWireframeCube( world );
+    render( world );
+
+    // Array of colours to be displayed in the histogram. Replace this with results from colour quantization
+    var dummyColors = [
         new THREE.Color( 1.0, 1.0, 1.0 ),
         new THREE.Color( 1.0, 1.0, 0.0 ),
         new THREE.Color( 1.0, 0.0, 1.0 ),
@@ -33,13 +86,30 @@ function onLoad() {
         new THREE.Color( 0.25, 0.25, 0.25 ),
         new THREE.Color( 0.5, 0.5, 0.5 ),
         new THREE.Color( 0.75, 0.75, 0.75 ),
-        new THREE.Color( 1.0, 0.6, 0.72 )
+        new THREE.Color( 0.5, 0.75, 0.5)
     ];
-    createWireframeCube( world );
+    //plotColors( world, dummyColors );
+}
 
-    plotColors( world, colors );
+/**
+ * Sort colours from 255 bins to the given number of bins
+ */
+function applyColorQuantisation(colors, numberOfBins) {
+    var quantisedColors = new Array(colors.length);
+    for (var i = 0; i < quantisedColors.length; i++) {
+        quantisedColors[i] = new Array(3);
+    }
 
-    render( world );
+    for (var i = 0; i < colors.length; i++) {
+        // Iterate over the red, green, and blue channels
+        for (var j = 0; j < 3; j++) {
+            var rawColor = colors[i][j];
+            quantisedColors[i][j] = Math.floor(rawColor / numberOfBins);
+            //console.log("Raw Color: ", rawColor, " Quantised Color: ", quantisedColor);
+        }
+    }
+
+    return quantisedColors;
 }
 
 /**
@@ -52,13 +122,12 @@ function onLoad() {
 function plotColors( world, colors ) {
     var pointGeometry, pointMaterial, pointMesh;
     var combinedGeometry, combinedMaterial, combinedMesh;
-    var sphereRadius = 0.075;
+    var sphereRadius = 0.1;
     var meshes = [];
 
     for ( var i = 0; i < colors.length; i++ ) {
         pointGeometry = new THREE.SphereGeometry( sphereRadius );
         pointMaterial = new THREE.MeshBasicMaterial(); // Changes made to this material will have no effect on the objects
-
         pointMesh = new THREE.Mesh(pointGeometry, pointMaterial);
 
         var position = findPositionOfColor( colors[i] );
@@ -82,15 +151,21 @@ function plotColors( world, colors ) {
     world.scene.add( combinedMesh );
 }
 
+/**
+ * Interpret a colour as a position in the 3D 0-1 space
+ * Converts 0-255 coordinates into 0-1 coordinates and shift by -0.5 to center around (0, 0, 0)
+ *
+ * @param color The colour to interpret as a position
+ * @returns {THREE.Vector3} The inferred position of the given colour
+ */
 function findPositionOfColor( color ) {
-    return new THREE.Vector3( color.r - 0.5, color.g - 0.5, color.b - 0.5 );
+    return new THREE.Vector3( (color[0] / 15) - 0.5, (color[1] / 15) - 0.5, (color[2] / 15)- 0.5 );
 }
 
 /**
  * Takes an array of meshes and merges them together for performance purposes
  *
  * @param meshes An array of meshes to be merged into one
- *
  * @return {*} The combined meshes
  */
 function mergeMeshes( meshes ) {
@@ -185,9 +260,7 @@ function findColorOfPoint( point ) {
  * @param world The world being rendered
  */
 function render( world ) {
-
     world.controls.update();
-
     world.renderer.render( world.scene, world.camera );
 
     // Anom. function used because render() has parameters
